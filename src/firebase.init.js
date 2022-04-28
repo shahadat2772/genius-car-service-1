dfbfa389aa59014fbd0ac6b47539ce01c0caf834
shahadat2772 <shahadat2772@gmailcom>
2022-04-28 10:52:19 +0600

    firebase config issue fixed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA6kncaOd-CbLFsA6ayaHCW_bidalvatyU",
+//   authDomain: "genius-car-services-1-2600a.firebaseapp.com",
+//   projectId: "genius-car-services-1-2600a",
+//   storageBucket: "genius-car-services-1-2600a.appspot.com",
+//   messagingSenderId: "671425596744",
+//   appId: "1:671425596744:web:615a9c93a9bcbae4f3377d",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyA6kncaOd-CbLFsA6ayaHCW_bidalvatyU",
-  authDomain: "genius-car-services-1-2600a.firebaseapp.com",
-  projectId: "genius-car-services-1-2600a",
-  storageBucket: "genius-car-services-1-2600a.appspot.com",
-  messagingSenderId: "671425596744",
-  appId: "1:671425596744:web:615a9c93a9bcbae4f3377d",
+  apiKey: "AIzaSyADKtpWq2rGEQYNHHa6TXyCWr5seD0K6D0",
+  authDomain: "genius-car-services-b294e.firebaseapp.com",
+  projectId: "genius-car-services-b294e",
+  storageBucket: "genius-car-services-b294e.appspot.com",
+  messagingSenderId: "963269149539",
+  appId: "1:963269149539:web:6458935202f7b3d6318956",
 };
 
 // Initialize Firebase
